2d52417a7e608ec902cc91387649783cddb52266
Bryce Mecum <petridish@gmail.com>
2023-02-21 16:08:18 -0900

    Start building out DoPut example codebase

--- basic-doput/client.cc
+++ basic-doput/client.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <thread>
 
 #include <arrow/result.h>
 #include <arrow/status.h>
@@ -8,16 +9,56 @@
 
 using arrow::Status;
 
+void SleepFor(double seconds)
+{
+	std::this_thread::sleep_for(
+		std::chrono::nanoseconds(static_cast<int64_t>(seconds * 1e9)));
+}
+
 Status RunMain(int argc, char** argv) {
-    return Status::OK();
+
+	arrow::flight::Location location;
+	ARROW_ASSIGN_OR_RAISE(location,
+						  arrow::flight::Location::ForGrpcTcp("localhost", 61234));
+
+	std::unique_ptr<arrow::flight::FlightClient> client;
+	ARROW_ASSIGN_OR_RAISE(client, arrow::flight::FlightClient::Connect(location));
+	std::cout << "Connected to " << location.ToString() << std::endl;
+
+	auto descriptor = arrow::flight::FlightDescriptor::Path({"test.parquet"});
+
+	while (1)
+	{
+		std::cout << "Getting list of Flights..." << std::endl;
+
+		std::unique_ptr<arrow::flight::FlightListing> listing;
+		ARROW_ASSIGN_OR_RAISE(listing, client->ListFlights());
+
+		while (true)
+		{
+			std::unique_ptr<arrow::flight::FlightInfo> flight_info;
+			ARROW_ASSIGN_OR_RAISE(flight_info, listing->Next());
+			if (!flight_info)
+				break;
+			std::cout << flight_info->descriptor().ToString() << std::endl;
+		}
+
+		SleepFor(1);
+	}
+
+	return Status::OK();
 }
 
 int main(int argc, char** argv) {
-    Status status = RunMain(argc, argv);
+	signal(SIGTERM, [](int i)
+		   { exit(i); });
 
-    if (!status.ok()) {
-        std::cerr << status << std::endl;
-    }
+	Status status = RunMain(argc, argv);
 
-    return 0;
+	if (!status.ok())
+	{
+		std::cerr << status << std::endl;
+	}
+
+	return 0;
 }
--- basic-doput/server.cc
+++ basic-doput/server.cc
@@ -12,15 +12,107 @@
 
 using arrow::Status;
 
-Status RunMain(int argc, char** argv) {
-  return Status::OK();
+class BasicDoPutFlightServer : public arrow::flight::FlightServerBase
+{
+public:
+	explicit BasicDoPutFlightServer(std::shared_ptr<arrow::fs::FileSystem> root)
+		: root_(std::move(root)) {}
+
+	arrow::Status ListFlights(
+		const arrow::flight::ServerCallContext &, const arrow::flight::Criteria *,
+		std::unique_ptr<arrow::flight::FlightListing> *listings) override
+	{
+		arrow::fs::FileSelector selector;
+		selector.base_dir = "/";
+		ARROW_ASSIGN_OR_RAISE(auto listing, root_->GetFileInfo(selector));
+
+		std::vector<arrow::flight::FlightInfo> flights;
+		for (const auto &file_info : listing)
+		{
+			if (!file_info.IsFile() || file_info.extension() != "parquet")
+				continue;
+			ARROW_ASSIGN_OR_RAISE(auto info, MakeFlightInfo(file_info));
+			flights.push_back(std::move(info));
+		}
+
+		*listings = std::unique_ptr<arrow::flight::FlightListing>(
+			new arrow::flight::SimpleFlightListing(std::move(flights)));
+		return arrow::Status::OK();
+	}
+
+private:
+	arrow::Result<arrow::flight::FlightInfo> MakeFlightInfo(
+		const arrow::fs::FileInfo &file_info)
+	{
+		ARROW_ASSIGN_OR_RAISE(auto input, root_->OpenInputFile(file_info));
+		std::unique_ptr<parquet::arrow::FileReader> reader;
+		ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(std::move(input),
+													 arrow::default_memory_pool(), &reader));
+
+		std::shared_ptr<arrow::Schema> schema;
+		ARROW_RETURN_NOT_OK(reader->GetSchema(&schema));
+
+		auto descriptor = arrow::flight::FlightDescriptor::Path({file_info.base_name()});
+
+		arrow::flight::FlightEndpoint endpoint;
+		endpoint.ticket.ticket = file_info.base_name();
+		arrow::flight::Location location;
+		ARROW_ASSIGN_OR_RAISE(location,
+							  arrow::flight::Location::ForGrpcTcp("localhost", port()));
+		endpoint.locations.push_back(location);
+
+		int64_t total_records = reader->parquet_reader()->metadata()->num_rows();
+		int64_t total_bytes = file_info.size();
+
+		return arrow::flight::FlightInfo::Make(*schema, descriptor, {endpoint}, total_records,
+											   total_bytes);
+	}
+
+	std::shared_ptr<arrow::fs::FileSystem> root_;
+};
+
+Status Serve(int argc, char **argv)
+{
+	if (argc <= 1)
+	{
+		return Status::Invalid("Must pass dataset directory to start Flight server. Exiting.");
+	}
+
+	std::string serve_directory = argv[1];
+
+	auto fs = std::make_shared<arrow::fs::LocalFileSystem>();
+	ARROW_RETURN_NOT_OK(fs->CreateDir(serve_directory));
+	auto root = std::make_shared<arrow::fs::SubTreeFileSystem>(serve_directory, fs);
+
+	arrow::flight::Location server_location;
+	ARROW_ASSIGN_OR_RAISE(server_location,
+						  arrow::flight::Location::ForGrpcTcp("0.0.0.0", 61234));
+
+	arrow::flight::FlightServerOptions options(server_location);
+	auto server = std::unique_ptr<arrow::flight::FlightServerBase>(
+		new BasicDoPutFlightServer(std::move(root)));
+
+	ARROW_RETURN_NOT_OK(server->Init(options));
+
+	std::cout << "🔈 Listening on port " << server->port() << std::endl;
+	std::cout << "🏹 Serving " << serve_directory << std::endl;
+
+	ARROW_RETURN_NOT_OK(server->Serve());
+
+	return Status::OK();
 }
 
 int main(int argc, char** argv) {
-  Status st = RunMain(argc, argv);
-  if (!st.ok()) {
-    std::cerr << st << std::endl;
-    return 1;
-  }
-  return 0;
+	signal(SIGTERM, [](int i)
+		   { exit(i); });
+
+	Status st = Serve(argc, argv);
+
+	if (!st.ok())
+	{
+		std::cerr << st << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
